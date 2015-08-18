4100fc223f3c437c0b2452f72b77788676dcc922
Jasmi <jasmi789@gmail.com>
2015-08-18 10:05:31 +0530

    Spinner dropdown item color changed

--- app/src/main/java/in/gravitykerala/aurissample/Transaction.java
+++ app/src/main/java/in/gravitykerala/aurissample/Transaction.java
@@ -3,7 +3,6 @@ package in.gravitykerala.aurissample;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -24,7 +23,6 @@ public class Transaction extends Activity {
     int position = -1;
 
 
-
     String[] operators = {
             "IDEA",
             "BSNL",
@@ -41,8 +39,6 @@ public class Transaction extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
 
         mClient = LoginActivity.mClient;
         mToDoTable = mClient.getTable(MobileTransactions.class);
