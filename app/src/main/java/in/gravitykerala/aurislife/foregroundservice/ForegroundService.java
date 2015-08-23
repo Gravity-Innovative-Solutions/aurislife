6fb042f8cef104b8b4734835a114bf2bea44b531
Prakash Joseph <expect112@gmail.com>
2015-08-23 23:54:17 +0530

    Reverted Notification icon update(Icon is not bad) -v1.1.0 Candidate

--- app/src/main/java/in/gravitykerala/aurislife/foregroundservice/ForegroundService.java
+++ app/src/main/java/in/gravitykerala/aurislife/foregroundservice/ForegroundService.java
@@ -72,7 +72,7 @@ public class ForegroundService extends Service {
                 .setContentTitle(getString(R.string.presc_upld))
                 .setTicker(getString(R.string.uplding_prgrs))
                 .setContentText(getString(R.string.uplding_prgrs))
-                .setSmallIcon(R.drawable.icon_notification)
+                .setSmallIcon(R.drawable.ic_not_aurislife)
                 .setProgress(0, 0, true)
                 .setContentIntent(pendingIntent)
                 .setOngoing(true).build();
@@ -104,7 +104,7 @@ public class ForegroundService extends Service {
                             .setContentTitle(getString(R.string.presc_upld))
                             .setTicker(getString(R.string.pres_upld_failed))
                             .setContentText(getString(R.string.pres_upld_failed_bcoz_nw_issue))
-                            .setSmallIcon(R.drawable.icon_notification)
+                            .setSmallIcon(R.drawable.ic_not_aurislife)
                             .setContentIntent(pendingIntent)
                             .setOngoing(false).build();
                     stopForeground(true);
@@ -154,7 +154,7 @@ public class ForegroundService extends Service {
                         .setContentTitle(getString(R.string.presc_upld))
                         .setTicker(getString(R.string.pres_upld_failed))
                         .setContentText(getString(R.string.pres_upld_failed_bcoz_nw_issue))
-                        .setSmallIcon(R.drawable.icon_notification)
+                        .setSmallIcon(R.drawable.ic_not_aurislife)
                         .setContentIntent(pendingIntent)
                         .setOngoing(false).build();
                 stopForeground(true);
@@ -180,7 +180,7 @@ public class ForegroundService extends Service {
                         .setContentTitle(getString(R.string.presc_upld))
                         .setTicker(getString(R.string.pres_upld_success))
                         .setContentText(getString(R.string.upld_finished))
-                        .setSmallIcon(R.drawable.icon_notification)
+                        .setSmallIcon(R.drawable.ic_not_aurislife)
                         .setContentIntent(pendingIntent)
                         .setOngoing(false).build();
 
